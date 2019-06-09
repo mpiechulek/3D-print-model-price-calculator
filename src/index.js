337aef37c208b99a8a932b1d403ff5249550654c
MP <47081376+mpiechulek@users.noreply.github.com>
2019-06-09 14:10:58 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
 //jshint esversion:6
 import { coLubie } from './js/app.js';
 import './sass/main.scss';
-
-// Scroll back button efect
-const btn = document.querySelector('.scrollBtn');
-
-window.addEventListener('scroll', () => {
-
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
-});
-
-// When the user clicks on the button, scroll to the top of the document
-
-btn.addEventListener("click", () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
