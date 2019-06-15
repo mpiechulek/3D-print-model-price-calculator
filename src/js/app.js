6e9925281b57cb53da646cd6ca14ce97fdd1590a
MP <47081376+mpiechulek@users.noreply.github.com>
2019-06-15 15:02:18 +0200

    final v2.3

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,73 @@
 //jshint esversion:6
 export const action = (() => {
 
-    let totalPrintCost;
+    let totalPrintCost,
+        rollPrice,
+        fillWeight,
+        _3DWeight,
+        powerCons,
+        priceKWh,
+        time_h,
+        time_m;
 
-    // Submit calculation
-    document.querySelector('.btn_submit').addEventListener('click', () => {
+    const warning = () => {
+        let sta_1 = document.querySelector('.warning_par').innerHTML = 'Please fill all fields with only numeric characters';
+        return sta_1;
+    };
 
-        //variables from inputs form
+    const getData = () => {
+        rollPrice = document.getElementById('fill-roll-price').value.replace(",", ".");
+        fillWeight = document.getElementById('fill-weight').value.replace(",", ".");
+        _3DWeight = document.getElementById('print-weight').value.replace(",", ".");
+        powerCons = document.getElementById('pow-consum').value.replace(",", ".");
+        priceKWh = document.getElementById('price-kwh').value.replace(",", ".");
+        time_h = document.getElementById('time-h').value.replace(",", ".");
+        time_m = document.getElementById('time-m').value.replace(",", ".");
+    };
 
-        const variables = {
-            rollPrice: document.getElementById('fill-roll-price').value.replace(",", "."),
-            fillWeight: document.getElementById('fill-weight').value.replace(",", "."),
-            _3DWeight: document.getElementById('print-weight').value.replace(",", "."),
-            powerCons: document.getElementById('pow-consum').value.replace(",", "."),
-            priceKWh: document.getElementById('price-kwh').value.replace(",", "."),
-            time_h: document.getElementById('time-h').value.replace(",", "."),
-            time_m: document.getElementById('time-m').value.replace(",", ".")
-        };
+    const calculate = () => {
 
-        //Fillament price
-        let weightRatio = variables._3DWeight / variables.fillWeight;
-        let totalFillPrice = variables.rollPrice * weightRatio;
+        let weightRatio = _3DWeight / fillWeight;
+        let totalFillPrice = rollPrice * weightRatio;
 
         //Print time
-        let time_minute = (variables.time_m / 60);
-        let totlalPrintTime = time_minute + variables.time_h;
+        let time_minute = (time_m / 60);
+        let totlalPrintTime = time_minute + time_h;
 
         //Power consumption
-        let powerPercentage = (variables.powerCons / 1000);
-        let powerPrice_1h = variables.priceKWh * powerPercentage;
+        let powerPercentage = (powerCons / 1000);
+        let powerPrice_1h = priceKWh * powerPercentage;
         let totalPowerCost = powerPrice_1h * totlalPrintTime;
 
         // total print cost
         totalPrintCost = Math.round((totalFillPrice + totalPowerCost) * 100) / 100;
+        console.log(`totalPrintCost= ${totalPrintCost}`);
+        return totalPrintCost;
+    };
+
+    // ========================================================================
+
+    // Submit calculation
+    document.querySelector('.btn_submit').addEventListener('click', () => {
+
+        getData();
+
+        let el = document.getElementsByClassName('form_inputs_input');
+
+        for (let i = 0; i < el.length; i++) {
+            if (el[i].value === '') {
+                console.log('Warning!');
+                warning();
+            } else {
+                console.log('Calculation');
+                calculate();
+            }
+        }
 
         if (!isNaN(totalPrintCost)) {
             document.getElementById('result').innerHTML = totalPrintCost;
         } else {
-            document.querySelector('.warning_par').innerHTML = 'Please fill all fields with only numeric characters';
+            warning();
         }
     });
 
@@ -45,6 +75,6 @@ export const action = (() => {
     document.querySelector('.btn_reset').addEventListener('click', () => {
         document.getElementById('result').innerHTML = '000';
         document.querySelector('.warning_par').innerHTML = '';
-        let totalPrintCoste = 0;
+        totalPrintCost = '000';
     });
 })();
