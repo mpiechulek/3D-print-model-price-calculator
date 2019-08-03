500f9656f841cce466a0c9f670e68817b2c7dc99
MP <47081376+mpiechulek@users.noreply.github.com>
2019-08-03 19:04:24 +0200

    Final 2.5

--- src/js/app.js
+++ src/js/app.js
@@ -20,32 +20,50 @@ export const action = (() => {
     const getData = () => {
 
         rollPrice = Math.abs(document.getElementById('fill-roll-price').value.replace(",", "."));
-        fillWeight =  Math.abs(document.getElementById('fill-weight').value.replace(",", "."));
-        _3DWeight =  Math.abs(document.getElementById('print-weight').value.replace(",", "."));
-        powerCons =  Math.abs(document.getElementById('pow-consum').value.replace(",", "."));
-        priceKWh =  Math.abs(document.getElementById('price-kwh').value.replace(",", "."));
-        time_h =  Math.abs(document.getElementById('time-h').value.replace(",", "."));
-        time_m =  Math.abs(document.getElementById('time-m').value.replace(",", "."));
+        fillWeight = Math.abs(document.getElementById('fill-weight').value.replace(",", "."));
+        _3DWeight = Math.abs(document.getElementById('print-weight').value.replace(",", "."));
+        powerCons = Math.abs(document.getElementById('pow-consum').value.replace(",", "."));
+        priceKWh = Math.abs(document.getElementById('price-kwh').value.replace(",", "."));
+        time_h = Math.abs(document.getElementById('time-h').value.replace(",", "."));
+        time_m = Math.abs(document.getElementById('time-m').value.replace(",", "."));
     };
 
     const calculate = () => {
 
-        if (rollPrice == '' || fillWeight == '' || _3DWeight == '' ||
-            powerCons == '' || priceKWh == '' || time_h == '' || time_m == '') {
+        if (rollPrice === '' || fillWeight === '' || _3DWeight === '' ||
+            powerCons === '' || priceKWh === '' || time_h === '' || time_m === '') {
 
             console.log('Warning!');
             return warning();
 
         } else {
-            let weightRatio = _3DWeight / fillWeight;
+            let weightRatio;
+            if (_3DWeight === 0 | fillWeight === 0) {
+                weightRatio = 0;
+            } else {
+                weightRatio = _3DWeight / fillWeight;
+            }
+
             let totalFillPrice = rollPrice * weightRatio;
+            let time_minute;
 
             //Print time
-            let time_minute = (time_m / 60);
+            if (time_m === 0) {
+                time_minute = 0;
+            } else {
+                time_minute = (time_m / 60);
+            }
+
             let totlalPrintTime = time_minute + time_h;
+            let powerPercentage;
 
             //Power consumption
-            let powerPercentage = (powerCons / 1000);
+            if (powerCons === 0) {
+                powerPercentage = 0;
+            } else {
+                powerPercentage = (powerCons / 1000);
+            }
+
             let powerPrice_1h = priceKWh * powerPercentage;
             let totalPowerCost = powerPrice_1h * totlalPrintTime;
 
@@ -67,12 +85,14 @@ export const action = (() => {
     // Submit calculation
     document.addEventListener('keypress', (event) => {
         if (event.keyCode === 13) {
+            document.querySelector('.warning_par').innerHTML = '';
             getData();
             calculate();
         }
     });
 
     document.querySelector('.btn_submit').addEventListener('click', () => {
+        document.querySelector('.warning_par').innerHTML = '';
         getData();
         calculate();
     });
